7cbb1233f0bdd71641797eebc9f15306550537ed
Noah Dietrich <52662093+njmdietrich@users.noreply.github.com>
2023-06-13 12:16:58 +0200

    Implemented max/argmax for RL scores
    Former-commit-id: 315373860fb1bf84427105dd60297df053a5507c

--- apps/recordlinkage/PartyRL.h
+++ apps/recordlinkage/PartyRL.h
@@ -198,6 +198,157 @@ private:
         return 29;
     }
   }
+
+  /**
+   * @brief Returns 0 if score1 is smaller than score2, 1 otherwise
+   *
+   * @param score1 p_score1:...
+   * @param score2 p_score2:...
+   * @return uint64_t
+   */
+  uint64_t compareScores(Score score1, Score score2) {
+    uint64_t compare1 = MUL(this, score1.numerator, score2.denominator);
+    uint64_t compare2 = MUL(this, score2.numerator, score1.denominator);
+    return CMP(this, compare1, compare2);
+  }
+
+  uint64_t* compareScores(
+    uint64_t* numerator1,
+    uint64_t* numerator2,
+    uint64_t* denominator1,
+    uint64_t* denominator2,
+    int count) {
+    uint64_t* compare1 = MUL(this, numerator1, denominator2, count);
+    uint64_t* compare2 = MUL(this, numerator2, denominator1, count);
+    return CMP(this, compare1, compare2, count);
+  }
+
+  /**
+  * Selects the maximum score from an array of shared Score s.
+  * @param scores an array of shared scores
+  * @param scoreCount the size of the score array
+  * @return The maximum score which was found in scores.
+  */
+  MaxScore computeMaxScore(Score *scores, uint32_t scoreCount){
+    /** MAIN IDEA:
+     Repeatedly compare the first half of the remaining scores to the second
+     half, using MUX to select the larger of the two scores.
+     Have an array contain secretly shared indices that are selected
+     with the same selection vector to also get the argmax.
+     */
+    bool remainderExists = false;
+    int arrayLength = scoreCount;
+    int halfLength;
+
+    if ((getPRole() == P1) || getPRole() == P2) {
+      // generate the initial data structures and values:
+      Score remainder;
+      uint64_t remainderIndex;
+      uint64_t* numerators = new uint64_t[scoreCount];
+      uint64_t* denominators = new uint64_t[scoreCount];
+      uint64_t* indices = new uint64_t[scoreCount];
+      uint64_t* selection;
+      // the tmp pointers are used to avoid memory leaks at various points
+      uint64_t *tmpNumerators, *tmpDenominators, *tmpIndices;
+      for (int i = 0; i < scoreCount; i++) {
+        numerators[i] = scores[i].numerator;
+        denominators[i] = scores[i].denominator;
+        indices[i] = createShare((uint64_t) i);
+      }
+      //iteratively halve the array by comparing the first with the second half:
+      while (arrayLength > 1) {
+        if (arrayLength % 2 == 1) {
+          if (remainderExists) {
+            /* we create new arrays with arrayLength+1 and store the remainder
+               in the last index:*/
+            tmpNumerators = numerators;
+            tmpDenominators = denominators;
+            tmpIndices = indices;
+            numerators = new uint64_t[arrayLength+1];
+            denominators = new uint64_t[arrayLength+1];
+            indices = new uint64_t[arrayLength+1];
+            memcpy(numerators, tmpNumerators, arrayLength);
+            memcpy(denominators, tmpDenominators, arrayLength);
+            memcpy(tmpIndices, indices, arrayLength);
+            delete[] tmpDenominators;
+            delete[] tmpNumerators;
+            delete[] tmpIndices;
+            numerators[arrayLength] = remainder.numerator;
+            denominators[arrayLength] = remainder.denominator;
+            indices[arrayLength] = remainderIndex;
+            remainderExists = false;
+            arrayLength++;
+          } else { // the remainder is stored for later:
+            remainder.denominator = denominators[arrayLength-1];
+            remainder.numerator = numerators[arrayLength-1];
+            remainderIndex = indices[arrayLength-1];
+            remainderExists = true;
+          }
+        }
+        halfLength = arrayLength / 2;
+        selection = compareScores(
+          numerators,
+          numerators + halfLength,
+          denominators,
+          denominators+halfLength,
+          halfLength
+        );
+        tmpNumerators = numerators;
+        tmpDenominators = denominators;
+        tmpIndices = indices;
+        numerators = MUX(this, numerators, numerators+halfLength, selection, halfLength);
+        denominators = MUX(this, denominators, denominators+halfLength, selection, halfLength);
+        indices = MUX(this, indices, indices+halfLength, selection, halfLength);
+        delete[] tmpNumerators;
+        delete[] tmpDenominators;
+        delete[] tmpIndices;
+        delete[] selection;
+        arrayLength = halfLength;
+      }
+      MaxScore max;
+      max.score.numerator = numerators[0];
+      max.score.denominator = denominators[0];
+      max.index = indices[0];
+      if (remainderExists) {
+        uint64_t selection = compareScores(max.score, remainder);
+        max.score.numerator = MUX(this, max.score.numerator, remainder.numerator, selection);
+        max.score.denominator = MUX(this, max.score.denominator, remainder.denominator, selection);
+        max.index = MUX(this, indices[0], remainderIndex, selection);
+      }
+      return max;
+    } else { // HELPER
+      //iteratively halve the array by comparing the first with the second half:
+      while (arrayLength > 1) {
+        if (arrayLength % 2 == 1) {
+          if (remainderExists) {
+            /* we create new arrays with arrayLength+1 and store the remainder
+               in the last index:*/
+            remainderExists = false;
+            arrayLength++;
+          } else { // the remainder is stored for later:
+            remainderExists = true;
+          }
+        }
+        halfLength = arrayLength / 2;
+        compareScores(nullptr, nullptr, nullptr, nullptr, halfLength);
+        MUX(this, nullptr, nullptr, nullptr, halfLength);
+        MUX(this, nullptr, nullptr, nullptr, halfLength);
+        MUX(this, nullptr, nullptr, nullptr, halfLength);
+        arrayLength = halfLength;
+      }
+      if (remainderExists) {
+        Score score;
+        compareScores(score, score);
+        MUX(this, 0, 0, 0);
+        MUX(this, 0, 0, 0);
+        MUX(this, 0, 0, 0);
+      }
+      MaxScore max;
+      return max;
+    }
+
+
+  }
 };
 
 #endif                                                      // PART_RL
